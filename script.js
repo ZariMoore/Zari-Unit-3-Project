c636f40997e520ef9ef586abd70118178fc95caa
ZariMoore <126017460+ZariMoore@users.noreply.github.com>
2023-02-21 11:44:53 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,46 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let optiononescreen = document.querySelector(".option-one-screen");
+let optiononeend = document.querySelector(".option-one-end");
+let optiononeyes = document.querySelector(".option-one");
+optiononeyes.onclick = function(){
+   optiononescreen.style.display = "block";
+   optiononeend.style.display = "block" ;
+};
+let optiontwoscreen= document.querySelector(".option-two-screen");
+
+let optiontwono = document.querySelector(".option-two");
+optiontwono.onclick = function(){
+    optiontwoscreen.style.display = "block";
+};
+let option1 = document.querySelector(".option1"); 
+option1.onclick = function(){
+    option1.style.display = "block";
+};
+let option2 = document.querySelector(".option2"); 
+option2.onclick = function(){
+    option2.style.display = "block";
+};
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
